aad71fa458dab3cdb81670b18d120a208cabcff3
woutvanveldhuizen <woutvanveldhuizen@gmail.com>
2026-07-08 21:05:52 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='clarity-offertemaker-v2.2dev2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./css/app.css','./css/pdf.css','./js/app.js','./js/storage.js','./js/locales.js','./js/database.js','./js/parser.js','./js/pdf.js','./assets/logo.jpg','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));

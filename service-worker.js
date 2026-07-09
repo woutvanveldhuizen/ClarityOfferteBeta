fd89f7fe9bb5738e9b47f3a39eec2b36e58ee8e9
woutvanveldhuizen <woutvanveldhuizen@gmail.com>
2026-07-09 22:09:40 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE = 'clarity-offertemaker-v3.0dev2';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './css/app.css',
+  './css/pdf.css',
+  './js/storage.js',
+  './js/locales.js',
+  './js/pdf.js',
+  './js/app.js',
+  './assets/logo.jpg',
+  './assets/icons/apple-touch-icon.png',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
